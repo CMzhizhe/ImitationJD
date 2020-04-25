fac8904a8e7f1f71b64edf3928ff75f17591d1aa
gxx <745570899@qq.com>
2020-04-25 14:47:32 +0800

    粗略的完成京东的效果

--- app/src/main/java/com/example/imitationjd/behavior/main/MainMyFrameLayoutBehavior.java
+++ app/src/main/java/com/example/imitationjd/behavior/main/MainMyFrameLayoutBehavior.java
@@ -18,6 +18,7 @@ import com.example.imitationjd.weiget.MyMainLinLerLayout;
 public class MainMyFrameLayoutBehavior extends CoordinatorLayout.Behavior<FrameLayout> {
     private String TAG = MainMyFrameLayoutBehavior.class.getSimpleName();
     private MyMainLinLerLayout myMainLinLerLayout;
+    private RecyclerView mainRecyclerView;
     public MainMyFrameLayoutBehavior() {
     }
 
@@ -34,16 +35,79 @@ public class MainMyFrameLayoutBehavior extends CoordinatorLayout.Behavior<FrameL
     @Override
     public void onNestedPreScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull FrameLayout child, @NonNull View target, int dx, int dy, @NonNull int[] consumed, int type) {
         super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type);
-        if (myMainLinLerLayout == null){
+        if (myMainLinLerLayout == null) {
             myMainLinLerLayout = coordinatorLayout.findViewById(R.id.first_my_linlayout);
         }
+
         if (target instanceof RecyclerView) {
+            float transY = child.getTranslationY() - dy;
+            if (target.getId() == R.id.main_header_recyclerview) {
+                RecyclerView recyclerView = (RecyclerView) target;
+                mainRecyclerView = recyclerView;
+                if (dy > 0) {//手指向上
+                    if (recyclerView.canScrollVertically(dy)) {
+                        recyclerView.scrollBy(0, dy);
+                    } else {
+                        if (child.getTranslationY() < -child.getHeight()) {
+                            transY = -child.getHeight();
+                        }
+                        child.setTranslationY(transY);
+                    }
+                }
+
+                if (dy < 0) {//手指向下
+                    if (child.getTranslationY() == 0) {
+                        child.setTranslationY(0);
+                        recyclerView.scrollBy(0, dy);
+                    } else {
+                        if (child.getTranslationY() > -child.getHeight()) {
+                            if (child.getTranslationY() > 0) {
+                                child.setTranslationY(0);
+                            } else {
+                                if (child.getTranslationY() + transY > 0) {
+                                    transY = 0;
+                                }
+                                consumed[1] = dy;
+                                child.setTranslationY(transY);
+                            }
+                            if (child.getTranslationY() == 0) {
+                                recyclerView.scrollBy(0, dy);
+                            }
+                        } else {
+                            recyclerView.scrollBy(0, dy);
+                        }
+                    }
+                }
+            } else if (target.getId() == R.id.fragment_recyclerview) {
+                if (dy > 0) {//手指向上
+                    if (myMainLinLerLayout.getTranslationY() != 0) {
+                        if (transY < -child.getHeight()) {
+                            transY = -child.getHeight();
+                        }
+                        child.setTranslationY(transY);
+                        consumed[1] = dy;
+                    }
+                }
+
+                if (dy < 0) {//手指向下
+                    if (transY > -child.getHeight() && transY < 0) {
+                        child.setTranslationY(transY);
+                    } else if (transY >0){
+                        mainRecyclerView.scrollBy(0,dy);
+                        child.setTranslationY(0);
+                    }
+                    consumed[1] = dy;
+                }
+            }
+        }
+
+        /*if (target instanceof RecyclerView) {
             if (target.getId() == R.id.main_header_recyclerview) {
                 RecyclerView mainRecyclerView = (RecyclerView) target;
                 float transY = child.getTranslationY() - dy;
+                Log.e(TAG,"transY:"+transY);
                 if (dy > 0 && !mainRecyclerView.canScrollVertically(dy)) {
                     child.setTranslationY(transY);
-                    consumed[1] = dy;
                 } else if (dy < 0) {
                     if (child.getTranslationY() < 0) {
                         child.setTranslationY(transY);
@@ -54,14 +118,14 @@ public class MainMyFrameLayoutBehavior extends CoordinatorLayout.Behavior<FrameL
                 }
             } else if (target.getId() == R.id.fragment_recyclerview) {
                 float transY = child.getTranslationY() - dy;
+                Log.e(TAG,"transY:"+transY);
                if (myMainLinLerLayout.getTranslationY()!=0){
                    child.setTranslationY(transY);
                    consumed[1] = dy;
                }else if (myMainLinLerLayout.getTranslationY() == 0){
                    child.setTranslationY(transY);
-                   consumed[1] = dy;
                }
             }
-        }
+        }*/
     }
 }
--- app/src/main/java/com/example/imitationjd/weiget/MyMainLinLerLayout.java
+++ app/src/main/java/com/example/imitationjd/weiget/MyMainLinLerLayout.java
@@ -5,6 +5,8 @@ import android.animation.AnimatorListenerAdapter;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
 
@@ -16,7 +18,9 @@ import androidx.core.view.NestedScrollingParent3;
 import androidx.core.view.ViewCompat;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.imitationjd.R;
 import com.example.imitationjd.viewmodel.MyViewModel;
@@ -34,6 +38,7 @@ public class MyMainLinLerLayout extends LinearLayout implements NestedScrollingP
     private Context mContext;
     private LinearLayout titleDescLayout;
     private boolean animatorIsRunning = false;
+
 
     public void setFirstActivity(AppCompatActivity firstActivity) {
         myViewModel = new ViewModelProvider(firstActivity.getViewModelStore(), ViewModelProvider.AndroidViewModelFactory.getInstance(firstActivity.getApplication())).get(MyViewModel.class);
@@ -62,6 +67,26 @@ public class MyMainLinLerLayout extends LinearLayout implements NestedScrollingP
         this.mContext = context;
     }
 
+    private int lastY = 0;
+    private int dy = 0;
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()){
+            case MotionEvent.ACTION_DOWN:{
+                lastY = (int) ev.getY();
+            }
+            break;
+
+            case MotionEvent.ACTION_MOVE:{
+                int currentY = (int) ev.getY();
+                dy = lastY - currentY;
+                lastY = currentY;
+            }
+            break;
+        }
+        return super.onInterceptTouchEvent(ev);
+    }
+
     @Override
     public void onNestedScroll(@NonNull View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int type, @NonNull int[] consumed) {
 
@@ -69,7 +94,19 @@ public class MyMainLinLerLayout extends LinearLayout implements NestedScrollingP
 
     @Override
     public boolean onStartNestedScroll(@NonNull View child, @NonNull View target, int axes, int type) {
-        return (axes & ViewCompat.SCROLL_AXIS_VERTICAL) != -1 && this.getTranslationY() == 0;
+        if ((axes & ViewCompat.SCROLL_AXIS_VERTICAL) != -1 && this.getTranslationY() ==0 ){
+            if (isDescExOpen && !animatorIsRunning && target instanceof RecyclerView && dy < 0){
+                if (!currentRecyclerView.canScrollVertically(dy)){
+                    return false;
+                }else {
+                    return true;
+                }
+            }else {
+                return true;
+            }
+        }else {
+            return false;
+        }
     }
 
     @Override
@@ -103,7 +140,7 @@ public class MyMainLinLerLayout extends LinearLayout implements NestedScrollingP
             if (isDescExOpen && !animatorIsRunning) {
                 animatorIsRunning = true;
                 ValueAnimator valueAnimator = ValueAnimator.ofFloat(1.0f, 0.1f);
-                valueAnimator.setDuration(800);
+                valueAnimator.setDuration(200);
                 valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
                     @Override
                     public void onAnimationUpdate(ValueAnimator animation) {
@@ -122,14 +159,15 @@ public class MyMainLinLerLayout extends LinearLayout implements NestedScrollingP
                 });
                 valueAnimator.start();
             }
+
         }
 
 
-        if (dy < 0 && !currentRecyclerView.canScrollVertically(dy)) {//手指向下
-            if (!isDescExOpen && !animatorIsRunning) {
+        if (dy < 0 ) {//手指向下
+            if (!isDescExOpen && !animatorIsRunning && !currentRecyclerView.canScrollVertically(dy)) {
                 animatorIsRunning = true;
                 ValueAnimator valueAnimator = ValueAnimator.ofFloat(0.1f, 1.0f);
-                valueAnimator.setDuration(800);
+                valueAnimator.setDuration(200);
                 valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
                     @Override
                     public void onAnimationUpdate(ValueAnimator animation) {
@@ -148,12 +186,12 @@ public class MyMainLinLerLayout extends LinearLayout implements NestedScrollingP
                 });
                 valueAnimator.start();
             }
-            consumed[1] = dy;
         }
-
     }
 
     private int getTitleDescHeight() {
         return mContext.getResources().getDimensionPixelOffset(R.dimen.first_title_desc);
     }
+
+
 }
